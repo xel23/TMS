000f259854035c9397d9d80a1490da3e68f0c179
igor <sawqe1@yandex.ru>
2019-12-01 14:14:12 +0300

    test ejs

--- server/app.js
+++ server/app.js
@@ -6,8 +6,10 @@ app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, '../client/views'));
 
+let data = [1, 2, 3, 5, 6, 11];
+
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {data: data});
 });
 
 app.listen(3000, function () {
