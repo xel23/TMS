9bab0a825809ab139cb2d7835b189a3f72c72b6d
яна <AvramenkoYana@yandex.ru>
2019-12-20 00:26:23 +0300

    add registration and authentication

--- server/app.js
+++ server/app.js
@@ -1,17 +1,75 @@
 const path = require('path');
 var express = require('express');
+var parser = require('body-parser');
+var cookieParser = require('cookie-parser');
 const config = require('./config');
+var session = require('express-session');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
+var bcrypt = require('bcryptjs');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+
 var app = express();
 
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, '../client/views'));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
-let data = [1, 2, 3, 5, 6, 11];
+// let data = [1, 2, 3, 5, 6, 11];
+//
+// app.get('/', function (req, res) {
+//     res.render('index', {data: data});
+// });
 
-app.get('/', function (req, res) {
-    res.render('index', {data: data});
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
 });
+
+app.get('*', function (req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+});
+
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+app.use('/', routes);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
---
+++ server/routes/index.js
@@ -0,0 +1,112 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+router.get('/', function (req, res, next) {
+    res.send('home');
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'Login'});
+});
+
+router.post('/login',
+    passport.authenticate('local', {failureRedirect: '/login', failureFlash: 'Invalid username or password'}),
+    function(req, res) {
+        req.flash('success', 'You are now logged in');
+        res.redirect('/dashboard')
+});
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy(function (username, password, done) {
+    User.getUserByUsername(username, function (err, user) {
+        if (err) throw err;
+        if (!user){
+            return done(null, false, {message: 'Unknown user'});
+        }
+        User.comparePassword(password, user.password, function (err, isMatch) {
+            if (err) return done(err);
+            if (isMatch){
+                return done(null, user);
+            } else {
+                return done(null, false, {message: 'Invalid password'})
+            }
+        })
+    });
+}));
+
+router.get('/register', function (req, res, next) {
+    res.render('register', {title: 'Register', errors: null});
+});
+
+router.post('/register', function (req, res, next) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password_2 = req.body.password_2;
+
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username field is required').notEmpty();
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('password_2', 'Passwords do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {title: 'Register', errors: errors})
+    } else {
+        var newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        });
+        User.createUser(newUser, function (err, user) {
+            if (err) throw err;
+            console.log(user);
+        });
+        req.flash('success', 'You are now registered and you can log in');
+        res.redirect('/login');
+    }
+});
+
+router.get('/profile', ensureAuthenticated, function (req, res, next) {
+    res.send('profile')
+});
+
+router.get('/task-list', ensureAuthenticated, function (req, res, next) {
+    res.render('list', {title: 'List'});
+});
+
+router.get('/dashboard', ensureAuthenticated, function (req, res, next) {
+    res.render('dashboard', {title: 'Dashboard'});
+});
+
+function ensureAuthenticated(req, res, next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
+router.get('/logout', function (req, res, next) {
+    req.logout();
+    req.flash('success', 'You are now logged out');
+    res.redirect('/login');
+});
+
+module.exports = router;
